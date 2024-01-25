f698c3aaf19eca02bd8c04f6fad610f9598941e2
Silu Hu <erinh816@gmail.com>
2024-01-25 08:33:25 -0500

    found another bug that some elements on page not clickable anymore, figured it out by changing z-index and position

--- particles.js
+++ particles.js
@@ -117,7 +117,7 @@ particlesJS('particles-js', {
 let count_particles, stats, update;
 stats = new Stats();
 stats.setMode(0);
-// stats.domElement.style.position = 'absolute';
+stats.domElement.style.position = 'absolute';
 stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
